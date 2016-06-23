63992de2cb6d69d114339deeadb7007f887fc61f
zhengxingjie <xingjie.zheng@zhenai.com>
2016-06-23 12:23:24 +0800

    add gradle

--- src/websocket/ChatWebSocket.java
+++ src/websocket/ChatWebSocket.java
@@ -1,28 +1,34 @@
 package websocket;
 
+import com.google.gson.Gson;
+
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+import static websocket.GetHttpSessionConfigurator.REMOTE_ADDRESS_AND_PORT;
+
 /**
  * Created by xj
  * on 2016/6/21.
  */
-@ServerEndpoint("/websocket")
+@ServerEndpoint(value = "/websocket", configurator = GetHttpSessionConfigurator.class)
 public class ChatWebSocket {
 
     private static int onlineCount = 0;
 
     private static CopyOnWriteArraySet<ChatWebSocket> chatWebSocketSet = new CopyOnWriteArraySet<>();
     private Session session;
+    private String host;
 
     @OnOpen
     public void onOpen(Session session) {
         this.session = session;
         chatWebSocketSet.add(this);
         addOnlineCount();
-        System.out.println("new connection, online count is " + getOnlineCount());
+        host = (String) session.getUserProperties().get(REMOTE_ADDRESS_AND_PORT);
+        System.out.println("new connection(" + host + "), online count is " + getOnlineCount());
     }
 
     @OnClose
@@ -40,16 +46,24 @@ public class ChatWebSocket {
 
     @OnMessage
     public void onMessage(String message, Session session) {
-        System.out.println("来自客户端" + session.getRequestURI() + "的消息:" + message);
+        Gson gson = new Gson();
+        if (host == null) {
+            host = (String) session.getUserProperties().get(REMOTE_ADDRESS_AND_PORT);
+        }
+        System.out.println("message from client" + host + ":" + message);
 
         for (ChatWebSocket item : chatWebSocketSet) {
             try {
-                item.sendMessage(message);
+                if (host != null && host.equals(item.session.getUserProperties().get(REMOTE_ADDRESS_AND_PORT))) {
+                    System.out.println("host:" + item.session.getUserProperties().get(REMOTE_ADDRESS_AND_PORT));
+                    item.sendMessage(message);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
                 continue;
             }
         }
+
     }
 
     private void sendMessage(String message) throws IOException {
---
+++ src/websocket/GetHttpSessionConfigurator.java
@@ -0,0 +1,34 @@
+package websocket;
+
+import javax.websocket.HandshakeResponse;
+import javax.websocket.server.HandshakeRequest;
+import javax.websocket.server.ServerEndpointConfig;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by xj
+ * on 2016/6/22.
+ */
+public class GetHttpSessionConfigurator extends ServerEndpointConfig.Configurator {
+
+    private static final String HOST = "host";
+    static final String REMOTE_ADDRESS_AND_PORT = "remote_address_and_port";
+
+    @Override
+    public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
+        super.modifyHandshake(sec, request, response);
+
+        Map<String, List<String>> header = request.getHeaders();
+        for (Map.Entry<String, List<String>> entry : header.entrySet()) {
+            if (HOST.equals(entry.getKey())) {
+                List<String> headersValues = entry.getValue();
+                if (headersValues != null && headersValues.size() > 0) {
+                    sec.getUserProperties().put(REMOTE_ADDRESS_AND_PORT, headersValues.get(0));
+                    System.out.println(headersValues.get(0));
+                }
+                break;
+            }
+        }
+    }
+}
